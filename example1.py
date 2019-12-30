bd49b6ff55cfa70a478bb8528898dc761ff51a76
patel77 <35058161+patel77@users.noreply.github.com>
2019-12-30 15:07:48 +0530

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,12 @@
+def hello():
+    print("hello")
+def example1():
+    print("example1")
+    print("to demostrate the toggle line")
+    print("this is third line and line",
+    "blaw al fjklasd faslkj asdkljl asdlkjfa third",
+    "aldsjkja ladsjlfk ldkasjlfk lajdsflk lakjdl third")
+    hello()
+def example2():
+    print("example2")
+example1()
---
+++ example2.py
@@ -0,0 +1,12 @@
+def example2():
+    print("A civilian fact-finding team,\
+    which includes prominent human rights activists hello third-one\
+    like John Dayal, Kavita Krishnan and Harsh Mander,\
+    has claimed to have proof that violence during the\
+    agitation was incited by the state police and that\
+    the Muslims in the area were tortured.hello  third-one")
+    print("hello")
+    print("world")
+    print("news")
+    print("paper")
+example2()
